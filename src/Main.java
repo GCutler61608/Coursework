634162d458a3288eba5d6c5903b51dde0800e859
61608 <gcutler.61608@farnborough.ac.uk>
2017-11-01 13:47:20 +0000

    test

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,17 @@
-package sample;
+package Model;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import sun.plugin2.util.ColorUtil;
-import javafx.scene.paint.Color;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 public class Main extends Application {
