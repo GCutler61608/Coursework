a4c770b853bf863eb1cef2a37d76c5b542fca004
61608 <gcutler.61608@farnborough.ac.uk>
2017-11-08 14:34:42 +0000

    test

---
+++ src/Model/MusicService.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class MusicService {
+        public static void selectAll(List<Item> destination, DatabaseConnection database) { }
+        public static Songs selectById(int id, DatabaseConnection database) { }
+        public static void save(Songs console, DatabaseConnection database) { }	// insert & update
+        public static void deleteById(int id, DatabaseConnection database) { }
+
+    }
+}
