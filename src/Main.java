44c08b6067cd6bdcab4e1df8c66cf926562bc224
gcutler.61608@farnborough.ac.uk <D@isy123>
2018-01-21 22:35:46 +0000

    cleaned up code to make it easier to understand

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ package Model;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -16,76 +18,128 @@ import java.util.Optional;
 
 public class Main extends Application {
 
-    public static DatabaseConnection database;
+    //public static DatabaseConnection database;
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        database = new DatabaseConnection("Songs.db");
+        //database = new DatabaseConnection("Songs.db");
 
         BorderPane root = new BorderPane();
 
-        Scene scene = new Scene(root, 1024, 768, Color.rgb(89, 89, 89));
+        Scene scene = new Scene(root, 1024, 768);
 
-        stage.setTitle("ok hi");
+        stage.setTitle("hii");
         stage.setScene(scene);
         scene.getStylesheets().add("stylesheet.css");
-        scene.setFill(Color.rgb(89, 89, 89));
-
 
         stage.setOnCloseRequest((WindowEvent we) -> exitPrompt(we));
+        stage.show(); //shows the scene
 
-        stage.show(); //THIS IS WHEN THE STAGE IS LAUNCHED
+        VBox leftPane = new VBox(20);
+        Button leftButton1 = new Button("I am left.");
+        leftPane.getChildren().add(leftButton1);
+        Button leftButton2 = new Button("I am left again.");
+        leftPane.getChildren().add(leftButton2);
+        root.setLeft(leftPane);
+        leftPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(leftPane, Pos.CENTER_LEFT);
+
+        VBox rightPane = new VBox(20);
+        Button rightButton1 = new Button("I am right.");
+        rightPane.getChildren().add(rightButton1);
+        Button rightButton2 = new Button("I am right again.");
+        rightPane.getChildren().add(rightButton2);
+        root.setRight(rightPane);
+        rightPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
+
+        VBox topPane = new VBox(20);
+        Button topButton1 = new Button("I am top.");
+        topPane.getChildren().add(topButton1);
+        Button topButton2 = new Button("I am top again.");
+        topPane.getChildren().add(topButton2);
+        root.setTop(topPane);
+        topPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
+
+        VBox bottomPane = new VBox(20);
+        Button bottomButton1 = new Button("I am bottom.");
+        bottomPane.getChildren().add(bottomButton1);
+        Button bottomButton2 = new Button("I am bottom again.");
+        bottomButton2.setOnAction((ActionEvent ae) -> openNewStage(root));;
+        bottomButton2.getStyleClass().add("funky_button");
+        bottomPane.getChildren().add(bottomButton2);
+        root.setBottom(bottomPane);
+        bottomPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(bottomPane, Pos.BOTTOM_CENTER);
+
+        VBox centerPane = new VBox(20);
+        Button centerButton1 = new Button("I am centre.");
+        centerPane.getChildren().add(centerButton1);
+        Button centerButton2 = new Button("I am centre again.");
+        centerPane.getChildren().add(centerButton2);
+        root.setCenter(centerPane);
+        centerPane.setAlignment(Pos.CENTER);
+        BorderPane.setAlignment(centerPane, Pos.CENTER);
 
 
-        VBox boxOfButtons = new VBox(10);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToWidth(true);
+        root.getChildren().add(scrollPane);
 
-        Button[] myButtons = new Button[5];
+        TilePane tilePane = new TilePane();
+        tilePane.prefWidthProperty().bind(root.widthProperty());
+        tilePane.prefHeightProperty().bind(root.heightProperty());
+        tilePane.setHgap(10);
+        tilePane.setVgap(10);
+        tilePane.setPadding(new Insets(20));
+        scrollPane.setContent(tilePane);
 
-        myButtons[0] = new Button("Button number one");
-        myButtons[0].setPrefSize(500, 50);
-        myButtons[0].setOnAction((ActionEvent ae) -> nothing(ae));
+        for (int i = 1; i <= 256; i++) {
+            Button exampleButton = new Button(Integer.toString(i));
+            exampleButton.setPrefSize(100, 100);
+            tilePane.getChildren().add(exampleButton);
+        }
 
-        myButtons[1] = new Button("Button number two");
-        myButtons[1].setPrefSize(300, 50);
-        myButtons[1].setOnAction((ActionEvent ae) -> nothing(ae));
 
-        myButtons[2] = new Button("Button number three");
-        myButtons[2].setPrefSize(300, 50);
-        myButtons[2].setOnAction((ActionEvent ae) -> nothing(ae));
 
-        myButtons[3] = new Button("Button number four");
-        myButtons[3].setPrefSize(200, 50);
-        myButtons[3].setOnAction((ActionEvent ae) -> nothing(ae));
-
-        myButtons[4] = new Button("Button number five");
-        myButtons[4].setPrefSize(200, 50);
-        myButtons[4].setOnAction((ActionEvent ae) -> nothing(ae));
-
-        boxOfButtons.getChildren().addAll(myButtons);
-
-        root.getChildren().add(boxOfButtons);
 
     }
 
-    public void nothing(ActionEvent ae) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("alertt");
-        alert.setHeaderText(null);
-        alert.setContentText("nothing here yet :))");
-        alert.showAndWait();
+    public static void openNewStage(Pane parent) {
+        importStage newStage = new importStage(parent);
     }
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
 
+    public void action(ActionEvent ae) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("alertt");
+        alert.setHeaderText("Header");
+        alert.setContentText("nothing here yet :))");
+        alert.showAndWait();
+    }
+
+    public static void doSomething(ActionEvent ae) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("Wow, you clicked the button with style!");
+        alert.showAndWait();
+    }
+
+
     public static void exitPrompt(WindowEvent we) {
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("ok bye");
-        alert.setHeaderText("confirm to exit");
+        alert.setTitle("Confirm");
+        alert.setHeaderText("Confirm to exit");
+        alert.setContentText("Click yes to close the program");
 
         Optional result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
---
+++ src/Model/importStage.java
@@ -0,0 +1,43 @@
+package Model;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
+
+public class importStage {
+
+    static Pane parent;
+
+    public importStage(Pane theParent) {
+
+        Stage stage = new Stage();
+        parent = theParent;
+        parent.setDisable(true);
+        start(stage);
+
+    }
+
+    public void start(Stage stage) {
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 1024, 768);
+        stage.setTitle("Import Music");
+        stage.setScene(scene);
+        stage.setOnCloseRequest((WindowEvent we) -> closeStage(stage));
+        stage.show();
+
+    }
+
+    public void closeStage(Stage stage) {
+
+        parent.setDisable(false);
+        stage.close();
+
+    }
+
+}
