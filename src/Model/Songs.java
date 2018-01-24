c26db545212b0a7f09dcd19aa66d75933ecc6830
gcutler.61608@farnborough.ac.uk <D@isy123>
2018-01-24 18:08:42 +0000

    getters & setters

--- src/Model/Songs.java
+++ src/Model/Songs.java
@@ -1,4 +1,67 @@
 package Model;
 
 public class Songs {
+    private int songID;
+    private int albumID;
+    private int songDuration;
+    private String songName;
+    private int featureID;
+    private int songDateAdded;
+
+    public Songs(int songID, int albumID, int songDuration, String songName, int featureID, int songDateAdded) {
+        this.songID = songID;
+        this.albumID = albumID;
+        this.songDuration = songDuration;
+        this.songName = songName;
+        this.featureID = featureID;
+        this.songDateAdded = songDateAdded;
+    }
+
+    public int getSongID() {
+        return songID;
+    }
+
+    public void setSongID(int songID) {
+        this.songID = songID;
+    }
+
+    public int getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(int albumID) {
+        this.albumID = albumID;
+    }
+
+    public int getSongDuration() {
+        return songDuration;
+    }
+
+    public void setSongDuration(int songDuration) {
+        this.songDuration = songDuration;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public int getFeatureID() {
+        return featureID;
+    }
+
+    public void setFeatureID(int featureID) {
+        this.featureID = featureID;
+    }
+
+    public int getSongDateAdded() {
+        return songDateAdded;
+    }
+
+    public void setSongDateAdded(int songDateAdded) {
+        this.songDateAdded = songDateAdded;
+    }
 }
