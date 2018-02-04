2e351d43543937c329dc2f7e879bdce503ef0884
gcutler.61608@farnborough.ac.uk <D@isy123>
2018-02-04 22:10:09 +0000

    Trying to add scroll pane

--- src/Model/importStage.java
+++ src/Model/importStage.java
@@ -1,13 +1,23 @@
 package Model;
 
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.scene.control.Button;
+
+
+
 
 import java.util.Optional;
+
+import static Model.Main.doSomething;
 
 public class importStage {
 
@@ -25,11 +35,32 @@ public class importStage {
     public void start(Stage stage) {
 
         Pane root = new Pane();
-        Scene scene = new Scene(root, 1024, 768);
+        Scene scene = new Scene(root, 819, 614);
+        scene.getStylesheets().add("resources/stylesheet.css");
         stage.setTitle("Import Music");
         stage.setScene(scene);
         stage.setOnCloseRequest((WindowEvent we) -> closeStage(stage));
         stage.show();
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToWidth(true);
+        root.getChildren().add(scrollPane);
+
+        TilePane tilePane = new TilePane();
+        tilePane.prefWidthProperty().bind(root.widthProperty());
+        tilePane.prefHeightProperty().bind(root.heightProperty());
+        tilePane.setHgap(10);
+        tilePane.setVgap(10);
+        tilePane.setPadding(new Insets(20));
+        scrollPane.setContent(tilePane);
+
+
+        for (int i = 1; i <= 256; i++) {
+            Button exampleButton = new Button(Integer.toString(i));
+            exampleButton.setPrefSize(100, 100);
+            exampleButton.setOnAction((ActionEvent ae) -> doSomething(ae));
+            tilePane.getChildren().add(exampleButton);
+        }
 
     }
 
